7e4e1fc6082d8c9d8dcb522d590a1d339a2539a6
FatalX2080 <maks.belozerov.06@mail.ru>
2025-03-08 01:21:37 +0300

    add clli new command

--- clli.py
+++ clli.py
@@ -1,17 +1,19 @@
 import subprocess
-import sys
+import sys, os
 import time
 
 sys.stderr.write("!!!THIS CLI WAS CREATED ONLY FOR QUICK LINUX CONFIGURATION!!!\n")
 
 START_FILE_NAME = "bot.service"
 PROMPT = "> "
+EXCLUDED_FILES = [".gitkeep"]
 
 menu = (
     "1: run",
     "2: stop",
     "3: status",
     "4: exit",
+    "5: clear data folder",
     "5: help",
 )
 
@@ -26,34 +28,43 @@ def run():
     print("| Reload")
     res = subprocess.run(["systemctl", "daemon-reload"], capture_output=True, text=True)
     if "Access denied" in res.stderr:
-        print("> Access denied")
+        print(r"\ Access denied")
         return
     print("| Enable")
     subprocess.run(["systemctl", "enable", START_FILE_NAME])
     print("| Start")
     subprocess.run(["systemctl", "start", START_FILE_NAME])
-    print("> Running success")
+    print(r"\ Running success")
 
 
 def stop():
+    #TODO костыль
     is_active =status()
     if not is_active[0]: return
     iex = is_active[1].index("PID: ")
     res = is_active[1][iex:].split()[1]
-    print("PPID:" + res)
+    print("PPID: " + res)
     subprocess.run(["systemctl", "disable", START_FILE_NAME])
     subprocess.run(["kill", res])
-    print("> Stopped process")
+    print(r"\ Stopped process")
 
 
 def status()->tuple:
-    res = subprocess.run(["systemctl", "status", "bot.service"], capture_output=True, text=True)
+    res = subprocess.run(["systemctl", "status", START_FILE_NAME], capture_output=True, text=True)
     if "could not be found" in res.stderr:
-        print("> Process isn't running")
+        print(r"\ Process isn't running")
         return False, None
-    else:
-        print("> It's running")
+    print(r"\ It's running")
     return True, res.stdout
+
+def clear_data_folder():
+    print("| Removing old data: ")
+    for file in os.listdir("."):
+        f_name = os.path.join("data", file)
+        if file in EXCLUDED_FILES:
+            print("| " + f_name)
+            os.remove(f_name)
+    print(r"\ End of deletion")
 
 def main():
     time.sleep(0.5)
@@ -70,9 +81,11 @@ def main():
             case "4":
                 exit()
             case '5':
+                clear_data_folder()
+            case '6':
                 cli_help()
             case _:
-                print("> Command not found")
+                print(r"\ Command not found")
                 cli_help()
 
 
